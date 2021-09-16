ce44c071fe694b4de27ed11babee280e8686af7d
fabio <fabio_santos_2001@outlook.com>
2021-09-16 10:33:00 -0300

    dynamic greetings

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import {Home} from './src/pages/Home';
 
 export default function App() {
   return (
     <>
+      <StatusBar barStyle="light-content" />
       <Home />
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, TextInput, StyleSheet, FlatList} from 'react-native';
 import {Button} from '../components/Button';
 import {SkillCard} from '../components/SkillCard';
@@ -6,13 +6,28 @@ import {SkillCard} from '../components/SkillCard';
 export function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [allSkills, setAllSkills] = useState([]);
+  const [greetings, setGreeting] = useState('');
 
   function handleNewSkill() {
     setAllSkills([...allSkills, newSkill]);
   }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+    console.log(currentHour);
+
+    if (currentHour <= 12) {
+      setGreeting('GoordMorning');
+    } else if (currentHour > 12 && currentHour < 18) {
+      setGreeting('Good Afternoon');
+    } else {
+      setGreeting('Good Night');
+    }
+  }, []);
   return (
     <View style={Styles.container}>
       <Text style={Styles.title}>Welcome, Fábio</Text>
+      <Text style={Styles.greetings}>{greetings}</Text>
       <TextInput
         style={Styles.input}
         placeholder="New Skill"
@@ -52,5 +67,8 @@ const Styles = StyleSheet.create({
     padding: 15,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#fff',
   },
 });
